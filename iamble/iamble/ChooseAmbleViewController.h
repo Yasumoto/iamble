7b510a15439f80b3c40610ce0d66d08e41c92916
Joe Smith <yasumoto7@gmail.com>
2012-06-01 23:55:01 -0700

    Add ChooseAmbleViewController to make a choice and display options.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ iamble/iamble/ChooseAmbleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseAmbleViewController.h
+//  iamble
+//
+//  Created by Joe Smith on 6/1/12.
+//  Copyright (c) 2012 iamble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseAmbleViewController : UIViewController
+
+@end
---
+++ iamble/iamble/ChooseAmbleViewController.m
@@ -0,0 +1,43 @@
+//
+//  ChooseAmbleViewController.m
+//  iamble
+//
+//  Created by Joe Smith on 6/1/12.
+//  Copyright (c) 2012 iamble. All rights reserved.
+//
+
+#import "ChooseAmbleViewController.h"
+
+@interface ChooseAmbleViewController ()
+
+@end
+
+@implementation ChooseAmbleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
