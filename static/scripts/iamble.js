8705407745695202eaff51c8be8a986f94295f21
Zach Szafran <zszafran@gmail.com>
2012-06-02 00:14:41 -0700

    Signed-off-by: Zach Szafran <zszafran@gmail.com> Initial HTML, JS, and CSS files.

---
+++ static/scripts/iamble.js
@@ -0,0 +1,26 @@
+(function($){
+  $.fn.iamble = function(o) {
+    return new $.iamble(this, o);
+  };
+
+  $.iamble = function(e, o) {
+    this.options = o || {};
+    this.target = $(e);
+    return this;
+  };
+
+  $.iamble.fn = $.iamble.prototype = {
+    iamble: '0.1'
+  };
+
+	$.iamble.fn.extend = $.iamble.extend = $.extend;
+
+  $.iamble.fn.extend({
+    'init': function() {
+      this.target.append(
+        $('<DIV>').text('HELLO WORLD!')
+      );
+      return this;
+    }
+  });
+});
