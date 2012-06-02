8fd690ca5b4218c8e291c820f539fba7f27412d2
Zach Szafran <zszafran@gmail.com>
2012-06-02 14:16:05 -0700

    added landing page.

--- appengine/handler.py
+++ appengine/handler.py
@@ -12,6 +12,7 @@ UNDER_CONSTRUCTION = """This site is currently under construction.
 Amble on back when you have a chance and check us out!"""
 
 HOME_TEMPLATE = 'templates/home.html'
+LANDING_TEMPLATE = 'templates/landing.html'
 REDIRECT_TEMPLATE = 'templates/redirect.html'
 
 
@@ -28,15 +29,28 @@ def RequiresLogin(handler_method):
 class BaseHandler(webapp.RequestHandler):
   """"""
 
-  @RequiresLogin
   def get(self):
     """"""
     template_params = template.get_params()
     template_params['warning_messages'].append(UNDER_CONSTRUCTION)
 
+    if users.get_current_user():
+      self.get_auth(template_params)
+    else:
+      self.get_noauth(template_params)
+
+  def get_auth(self, template_params):
+    """"""
+
     template.render_template(self, HOME_TEMPLATE, template_params)
 
+  def get_noauth(self, template_params):
+    """"""
+    template.render_template(self, LANDING_TEMPLATE, template_params)
+
+  @RequiresLogin
   def post(self):
+    """"""
     ret = dict()
     ret['message'] = 'hello world'
     response = json.dumps(ret)
--- appengine/profiles/login.py
+++ appengine/profiles/login.py
@@ -1,11 +1,14 @@
 # __author__ = russ@iamble
 
 import models
+<<<<<<< HEAD
 import handler
 from utils.template import render_template
+=======
+from utils import template
+>>>>>>> added landing page.
 from google.appengine.api import users
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
 
 class LoginHandler(webapp.RequestHandler):
   """Handler for Login page."""
@@ -15,16 +18,21 @@ class LoginHandler(webapp.RequestHandler):
   def get(self):
     """Handles get request for logins."""
     user = users.get_current_user()
+    template_params = template.get_params()
+
     if user:
       ambler = models.Ambler.get_by_id(user.email())
       if ambler:
-        self.redirect('/iamble')
-      else: 
-        self.redirect('/create_account?user=%s' % user.email())
-    else:  # If not authenticated, redirect to Google login page
+        # Redirect to core site
+        template_params['messages'].append('You are logged in.')
+      else:
+        self.redirect('/create_account?user=%s' % user.email()) #needs an escape
+        # Account creation includes all Oauth generation
+    else:
       redirect_url = users.create_login_url(dest_url='/login')
       self.redirect(redirect_url)
-  
+    template.render_template(self, self.LOGIN_TEMPLATE, template_params)
+
   def post(self):
     """Handles post for login page."""
     # We don't handle posted login info. Get off my lawn.
