ac4821d27f7014e3e02957f67e5b98295baa0770
James <james.d.meador@gmail.com>
2012-06-02 02:54:47 -0700

    Adding basic oauth services for Facebook,twitter and a simple handler to log the codes to console.

--- appengine/main.py
+++ appengine/main.py
@@ -7,8 +7,8 @@ from google.appengine.ext import webapp
 
 URL_MAPPINGS = [
     ('/', handler.BaseHandler),
-    (oauth.OAuth2Handler.URL_PATH, oauth.OAuth2Handler)]
-
+    (oauth.OAuth2Handler.URL_PATH, oauth.OAuth2Handler),
+    (oauth.OAuth2CallbackHandler.URL_PATH, oauth.OAuth2CallbackHandler)]
 
 def main():
   application = webapp.WSGIApplication(URL_MAPPINGS, debug=True)
