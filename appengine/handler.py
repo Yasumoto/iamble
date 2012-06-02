3827645af927534010af0833472cf5744297da5d
Zach Szafran <zszafran@gmail.com>
2012-06-02 00:42:18 -0700

    Signed-off-by: Zach Szafran <zszafran@gmail.com> DefaultHandler renders home.html template.

--- appengine/handler.py
+++ appengine/handler.py
@@ -1,8 +1,17 @@
 import utils
+
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
+
+UNDER_CONSTRUCTION = """'This site is currently under construction.
+Amble on back when you have a chance and check us out!'"""
+
+HOME_TEMPLATE = 'templates/home.html'
 
 class DefaultHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('This site is currently under construction. Amble on back when you have a chance and check us out!')
+    template_params = dict()
+    template_params['messages'] = list()
+    template_params['messages'].append(UNDER_CONSTRUCTION)
+    self.response.out.write(template.render(HOME_TEMPLATE, template_params))
