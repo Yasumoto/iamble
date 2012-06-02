a84b6e3cfec566ece78e7ccf27ca2cb993671202
Joe Smith <yasumoto7@gmail.com>
2012-06-01 23:51:33 -0700

    Add LoginViewController base to handle user auth.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ iamble/iamble/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  iamble
+//
+//  Created by Joe Smith on 6/1/12.
+//  Copyright (c) 2012 iamble. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
