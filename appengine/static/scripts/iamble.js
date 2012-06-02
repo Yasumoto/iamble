6164d681b16abd8b6360667188db507eefaa4f95
Zach Szafran <zszafran@gmail.com>
2012-06-02 11:03:47 -0700

    update to status message, added ajax POST to home.html

--- appengine/static/scripts/iamble.js
+++ appengine/static/scripts/iamble.js
@@ -23,6 +23,37 @@
     'render': function() {
 
       return this;
+    },
+    'message': function(msg, type) {
+      var statusMessage = $(document.createElement('DIV'))
+        .addClass('status_message')
+        .addClass(type)
+        .text(msg)
+        .hide();
+
+      this.options.messages.fadeOut('fast', function() {
+        $(this).empty().show().append(statusMessage);
+        statusMessage.fadeIn();
+      });
+      return this;
+    },
+    'success': function(msg) {
+      return this.message(msg, 'success');
+    },
+    'info': function(msg) {
+      return this.message(msg, 'info');
+    },
+    'warning': function(msg) {
+      return this.message(msg, 'warning');
+    },
+    'error': function(msg) {
+      return this.message(msg, 'error');
+    },
+    'bind': function(funct) {
+      var self = this;
+      return function() {
+        funct.apply(self, arguments);
+      };
     }
   });
 })(jQuery);
