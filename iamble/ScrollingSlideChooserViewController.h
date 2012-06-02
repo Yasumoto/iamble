5f724a41ac68f353db10d740407b9bc504393846
Joe Smith <yasumoto7@gmail.com>
2012-06-01 23:58:31 -0700

    Add more skeleton files.
    Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ iamble/ScrollingSlideChooserViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScrollingSlideChooserViewController.h
+//  iamble
+//
+//  Created by Joe Smith on 6/1/12.
+//  Copyright (c) 2012 iamble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollingSlideChooserViewController : UIViewController
+
+@end
---
+++ iamble/iamble/iambleServiceConnection.m
@@ -0,0 +1,13 @@
+//
+//  iambleServiceConnection.m
+//  iamble
+//
+//  Created by Joe Smith on 6/1/12.
+//  Copyright (c) 2012 iamble. All rights reserved.
+//
+
+#import "iambleServiceConnection.h"
+
+@implementation iambleServiceConnection
+
+@end
