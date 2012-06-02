00d8e36dfcc147c8e7a3fb6a02e223124b70743a
Russell White <russ.white88@gmail.com>
2012-06-02 14:50:12 -0700

    Initial submit of data modeling
    Setting up data structures and adding data directory.

---
+++ appengine/data/data_models.py
@@ -0,0 +1,35 @@
+# __author__ = russ@iamble
+
+from profiles import models as profile_models
+from google.appengine.ext import ndb
+
+
+class Source(ndb.Model):
+  """Source of signal information."""
+  service = ndb.StringProperty()
+
+class Coordinate(ndb.Model):
+  """Lat and Long object."""
+  lat = ndb.FloatProperty()
+  long = ndb.FloatProperty()
+
+
+class Checkin(ndb.Model):
+  """Checkin objects used as signals."""
+  source = ndb.KeyProperty(kind=Source)
+  who = ndb.KeyProperty(kind=profile_models.Ambler)
+  friend = ndb.StringProperty()
+  place = ndb.KeyProperty(kind=Place)
+  coordinate = ndb.StructuredProperty(Coordinate)  # keep duplicated for speedy index
+  when = ndb.DateTimeProperty()
+  data = ndb.StringProperty()
+  signal_value = ndb.IntegerProperty()
+
+
+class Place(ndb.Model):
+  """A spot to go eat."""
+  coordinate = ndb.StructuredProperty(Coordinate)
+  name = ndb.StringProperty()
+  type = ndb.StringProperty(choices=['coffee', 'quick', 'sit-down'])
+  cost = ndb.IntegerProperty(choices=[1,2,3,4,5])
+  mentions = ndb.IntegerProperty()
