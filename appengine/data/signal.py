c72742f598be76ea4358f93e2e0446afb0d4b57b
Russell White <russ.white88@gmail.com>
2012-06-03 05:23:56 -0700

    Refactor out some confusing code
    Consolidated some small useless functions into a larger one, prepared
    for places api changes.

--- appengine/data/signal.py
+++ appengine/data/signal.py
@@ -37,29 +37,32 @@ class SignalEngine(object):
 
   def FullDataProcess(self):
     """Fires a full data collection and parse process."""
-    self.CollectRawJSON()
+    json_checkins = data_utils.GetCheckinsForUser(self.ambler)
+    self.ProcessCheckins(json_checkins)
     caches.SetPersistentCache(self.ambler, 10)
     top_suggestion = caches.GetPersistentCache(self.ambler, 1)
     return top_suggestion
 
-  def CollectRawJSON(self):
+  def ProcessCheckins(self, json_checkins):
     """Does a massive data gather on all activated services."""
-    json_checkins = data_utils.GetCheckinsForUser(self.ambler)
+    parsed_signal = {}
     for checkin in json_checkins:
-      logging.info(checkin)
       try:
-        if constants.FOURSQUARE_FOOD_PARENT in [i.lower() for i in checkin['data']['categories']['parents']]:
-          self.ProcessCheckin(checkin)
+        source = checkin['oembed']['provider_name']
       except KeyError:
+        logging.info('missed on fb and 4s for %s', checkin)
+        source = None
+      if source == 'facebook':
+        # call jimmy app
         pass
-
-  def ProcessCheckin(self, signal):
-    """Takes signal as an argument representing an item that is to be analyzed."""
-    parsed_signal = self.ParseJSON(signal)
-    place = self.FindPlace(parsed_signal)
-    signal_object = self.CheckForDuplicateCheckin(parsed_signal, place)
-    if not signal_object:
-      self.AddCheckinToPlace(parsed_signal, place)
+      elif source == 'foursquare':
+        if constants.FOURSQUARE_FOOD_PARENT in [i.lower() for i in checkin['data']['categories']['parents']]:
+          parsed_signal = self.ParseFoursquareJSON()
+      if parsed_signal:
+        place = self.FindPlace(parsed_signal)
+        signal_object = self.CheckForDuplicateCheckin(parsed_signal, place)
+        if not signal_object:
+          self.AddCheckinToPlace(parsed_signal, place)
 
   def FindPlace(self, parsed_signal):
     """Associates a possible place with a passed in signal."""
@@ -71,15 +74,6 @@ class SignalEngine(object):
     if not place:
       place = self.InsertNewPlace(parsed_signal).get()
     return place
-
-  def CheckForDuplicateCheckin(self, parsed_signal, place):
-    """Check for duplicate checkins."""
-    duplicate = data_models.Checkin.query(ancestor=place.key)
-    duplicate = duplicate.filter(data_models.Checkin.who == users.get_current_user.email())
-    duplicate = duplicate.filter(data_models.Checkin.friend == parsed_signal['friend'])
-    duplicate = duplicate.filter(data_models.Checkin.coordinate.lat == parsed_signal['lat'])
-    duplicate = duplicate.filter(data_models.Checkin.coordinate.lng == parsed_signal['lng'])
-    return duplicate.get()
 
   def InsertNewPlace(self, parsed_signal):
     """Adds a place we haven't seen before to datastore."""
@@ -97,6 +91,15 @@ class SignalEngine(object):
     new_place.mentions = parsed_signal['mentions']
     return new_place.put()
 
+  def CheckForDuplicateCheckin(self, parsed_signal, place):
+    """Check for duplicate checkins."""
+    duplicate = data_models.Checkin.query(ancestor=place.key)
+    duplicate = duplicate.filter(data_models.Checkin.who == users.get_current_user.email())
+    duplicate = duplicate.filter(data_models.Checkin.friend == parsed_signal['friend'])
+    duplicate = duplicate.filter(data_models.Checkin.coordinate.lat == parsed_signal['lat'])
+    duplicate = duplicate.filter(data_models.Checkin.coordinate.lng == parsed_signal['lng'])
+    return duplicate.get()
+
   def AddCheckinToPlace(self, parsed_signal, place):
     """Adds a child checkin object to a place entity."""
     new_checkin_coordinate = profile_models.Coordinate
@@ -110,8 +113,8 @@ class SignalEngine(object):
     new_checkin.data = parsed_signal['data']
     new_checkin.put()
 
-  def ParseJSON(self, signal):
-    """Parses an input JSON signal."""
+  def ParseFoursquareJSON(self, signal):
+    """Parses an input JSON signal from Foursquare."""
     parsed_signal = dict()
     parsed_signal['name'] = signal['data']['venue']['name']
     parsed_signal['lat'] = signal['data']['location']['lat']
@@ -125,8 +128,24 @@ class SignalEngine(object):
     parsed_signal['when'] = signal['data']['createdAt']
     parsed_signal['data'] = signal['data']['user']['trips']
     return parsed_signal
+  
+  def ParseFacebookJSON(self, signal):
+    """Parses an input JSON signal from Facebook."""
+    parsed_signal = dict() # these are all wrong
+    parsed_signal['name'] = signal['data']['venue']['name']
+    parsed_signal['lat'] = signal['data']['location']['lat']
+    parsed_signal['lng'] = signal['data']['location']['lng']
+    parsed_signal['address'] = signal['data']['location']['address']
+    parsed_signal['city'] = signal['data']['location']['city']
+    parsed_signal['state'] = signal['data']['location']['state']
+    parsed_signal['type'] = self._FoursquareDetermineType(signal)
+    parsed_signal['friend'] = '%s %s' % (signal['data']['user']['firstName'], signal['data']['user']['lastName'])
+    parsed_signal['mentions'] = signal['data']['venue']['stats']
+    parsed_signal['when'] = signal['data']['createdAt']
+    parsed_signal['data'] = signal['data']['user']['trips']
+    return parsed_signal
 
-  def _DetermineType(self, signal):
+  def _FoursquareDetermineType(self, signal):
     """Determines the signal type using our nifty constants."""
     for quick_string in constants.QUICK_STRINGS:
       if quick_string in [i.lower() for i in [signal['data']['categories']['name'].split()]]:
