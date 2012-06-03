cf8fdbb9e6ab3fe1b8ec4d00ce286b9424a06d68
James <james.d.meador@gmail.com>
2012-06-03 05:13:59 -0700

    adding places queries

--- appengine/config.py
+++ appengine/config.py
@@ -1,11 +1,11 @@
 CLIENT_ID = '4eed71589ff0a822458e50db4b9ebb42'
 CLIENT_SECRET = 'd13bc8daa661cd7ea6bb3917ba687d29'
 
-CALLBACK_URI = 'https://ambleapp.appspot.com/oauth_callback'
+#CALLBACK_URI = 'https://ambleapp.appspot.com/oauth_callback'
 
 LOGIN_URL = '/login'
 
-#CALLBACK_URI = 'http://localhost:8080/oauth_callback'
+CALLBACK_URI = 'http://localhost:8080/oauth_callback'
 
 SINGLY_OAUTH_BASE_URL = 'https://api.singly.com/oauth/authorize'
 
@@ -21,3 +21,6 @@ SINGLY_API_PROFILES = 'https://api.singly.com/v0/profiles'
 SINGLY_API_MY_CHECKINS = 'https://api.singly.com/v0/types/checkins'
 
 SINGLY_API_CHECKIN_FEED = 'https://api.singly.com/v0/types/checkins_feed'
+
+GOOGLE_API_KEY = 'AIzaSyAITJKrjGM6TPINct8KLa7dty3x2xErNJw'
+GOOGLE_PLACES_API = 'https://maps.googleapis.com/maps/api/place/search/json'
