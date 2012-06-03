88ed924efbad9a672c428dfafbab6d8335007288
Russell White <russ.white88@gmail.com>
2012-06-03 12:49:05 -0700

    Added Facebook parsing logic
    This was a tough one. Added logic to allow parsing of Facebook JSON
    data. Almost all changes in signal.py.

--- appengine/data/signal.py
+++ appengine/data/signal.py
@@ -4,8 +4,11 @@ import constants
 import caches
 import data_models
 import data_utils
+import datetime
 import logging
 import handler
+import json
+from data import data_utils
 from profiles import models as profile_models
 from google.appengine.api import users
 
@@ -53,11 +56,15 @@ class SignalEngine(object):
         logging.info('missed on fb and 4s for %s', checkin)
         source = None
       if source == 'facebook':
-        # call jimmy app
-        pass
+        lat = checkin['oembed']['lat']
+        lng = checkin['oembed']['lng']
+        name = checkin['oembed']['title']
+        google_signal = json.loads(data_utils.GetGooglePlace(lat, lng, name))
+        if not google_signal['status'] == 'ZERO_RESULTS':
+          parsed_signal = self.ParseFacebookJSON(google_signal, checkin)
       elif source == 'foursquare':
         if constants.FOURSQUARE_FOOD_PARENT in [i.lower() for i in checkin['data']['categories']['parents']]:
-          parsed_signal = self.ParseFoursquareJSON()
+          parsed_signal = self.ParseFoursquareJSON(signal)
       if parsed_signal:
         place = self.FindPlace(parsed_signal)
         signal_object = self.CheckForDuplicateCheckin(parsed_signal, place)
@@ -77,10 +84,10 @@ class SignalEngine(object):
 
   def InsertNewPlace(self, parsed_signal):
     """Adds a place we haven't seen before to datastore."""
-    new_place_coordinate = profile_models.Coordinate
+    new_place_coordinate = profile_models.Coordinate()
     new_place_coordinate.lat = parsed_signal['lat']
     new_place_coordinate.lng = parsed_signal['lng']
-    new_place = data_models.Place
+    new_place = data_models.Place()
     new_place.coordinate = new_place_coordinate
     new_place.name = parsed_signal['name']
     new_place.address = parsed_signal['address']
@@ -94,7 +101,7 @@ class SignalEngine(object):
   def CheckForDuplicateCheckin(self, parsed_signal, place):
     """Check for duplicate checkins."""
     duplicate = data_models.Checkin.query(ancestor=place.key)
-    duplicate = duplicate.filter(data_models.Checkin.who == users.get_current_user.email())
+    duplicate = duplicate.filter(data_models.Checkin.who == self.ambler.key)
     duplicate = duplicate.filter(data_models.Checkin.friend == parsed_signal['friend'])
     duplicate = duplicate.filter(data_models.Checkin.coordinate.lat == parsed_signal['lat'])
     duplicate = duplicate.filter(data_models.Checkin.coordinate.lng == parsed_signal['lng'])
@@ -102,11 +109,11 @@ class SignalEngine(object):
 
   def AddCheckinToPlace(self, parsed_signal, place):
     """Adds a child checkin object to a place entity."""
-    new_checkin_coordinate = profile_models.Coordinate
+    new_checkin_coordinate = profile_models.Coordinate()
     new_checkin_coordinate.lat = parsed_signal['lat']
     new_checkin_coordinate.lng = parsed_signal['lng']
     new_checkin = data_models.Checkin(parent=place.key)
-    new_checkin.who = users.get_current_user().email()
+    new_checkin.who = self.ambler.key
     new_checkin.friend = parsed_signal['friend']
     new_checkin.coordinate = new_checkin_coordinate
     new_checkin.when = parsed_signal['when']
@@ -125,35 +132,51 @@ class SignalEngine(object):
     parsed_signal['type'] = self._DetermineType(signal)
     parsed_signal['friend'] = '%s %s' % (signal['data']['user']['firstName'], signal['data']['user']['lastName'])
     parsed_signal['mentions'] = signal['data']['venue']['stats']
-    parsed_signal['when'] = signal['data']['createdAt']
+    parsed_signal['when'] = datetime.datetime.fromtimestamp(signal['data']['createdAt'])
     parsed_signal['data'] = signal['data']['user']['trips']
     return parsed_signal
   
-  def ParseFacebookJSON(self, signal):
+  def ParseFacebookJSON(self, google_signal, signal):
     """Parses an input JSON signal from Facebook."""
+    logging.info(google_signal)
     parsed_signal = dict() # these are all wrong
-    parsed_signal['name'] = signal['data']['venue']['name']
-    parsed_signal['lat'] = signal['data']['location']['lat']
-    parsed_signal['lng'] = signal['data']['location']['lng']
-    parsed_signal['address'] = signal['data']['location']['address']
-    parsed_signal['city'] = signal['data']['location']['city']
-    parsed_signal['state'] = signal['data']['location']['state']
-    parsed_signal['type'] = self._FoursquareDetermineType(signal)
-    parsed_signal['friend'] = '%s %s' % (signal['data']['user']['firstName'], signal['data']['user']['lastName'])
-    parsed_signal['mentions'] = signal['data']['venue']['stats']
-    parsed_signal['when'] = signal['data']['createdAt']
-    parsed_signal['data'] = signal['data']['user']['trips']
+    parsed_signal['name'] = signal['oembed']['title']
+    parsed_signal['lat'] = signal['oembed']['lat']
+    parsed_signal['lng'] = signal['oembed']['lng']
+    parsed_signal['address'] = google_signal['results'][0]['vicinity'].split(',')[0]
+    parsed_signal['city'] = signal['data']['place']['location']['city']
+    parsed_signal['state'] = signal['data']['place']['location']['state']
+    parsed_signal['type'] = self._FacebookDetermineType(signal)
+    parsed_signal['friend'] = signal['oembed']['author_name']
+    parsed_signal['mentions'] = 0
+    logging.info('datetime throwing error %s', signal['at'])
+    parsed_signal['when'] = datetime.datetime.fromtimestamp((signal['at']/1000))
+    parsed_signal['data'] = signal['data']['message']
+    parsed_signal['likes'] = signal['data']['likes']['count']
     return parsed_signal
 
   def _FoursquareDetermineType(self, signal):
-    """Determines the signal type using our nifty constants."""
+    """Determines the signal type using our nifty constants for Foursquare."""
     for quick_string in constants.QUICK_STRINGS:
-      if quick_string in [i.lower() for i in [signal['data']['categories']['name'].split()]]:
+      if quick_string in [i.lower() for i in signal['data']['categories']['name'].split()]:
         return 'quick'
     for sit_down_string in constants.SIT_DOWN_STRINGS:
-      if sit_down_string in [i.lower() for i in [signal['data']['categories']['name'].split()]]:
+      if sit_down_string in [i.lower() for i in signal['data']['categories']['name'].split()]:
         return 'sit-down'
     for coffee_string in constants.COFFEE_STRINGS:
-      if coffee_string in [i.lower() for i in [signal['data']['categories']['name'].split()]]:
+      if coffee_string in [i.lower() for i in signal['data']['categories']['name'].split()]:
+        return 'coffee'
+    return 'default'
+    
+  def _FacebookDetermineType(self, signal):
+    """Determines the signal type using our nifty constants for Facebook."""
+    for quick_string in constants.QUICK_STRINGS:
+      if quick_string in [i.lower() for i in signal['oembed']['title'].split()]:
+        return 'quick'
+    for sit_down_string in constants.SIT_DOWN_STRINGS:
+      if sit_down_string in [i.lower() for i in signal['oembed']['title'].split()]:
+        return 'sit-down'
+    for coffee_string in constants.COFFEE_STRINGS:
+      if coffee_string in [i.lower() for i in signal['oembed']['title'].split()]:
         return 'coffee'
     return 'default'
