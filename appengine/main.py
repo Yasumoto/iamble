f10cfb52822b15ab19b5f0119aa708639bd211f1
Zach Szafran <zszafran@gmail.com>
2012-06-02 17:14:14 -0700

    logout handler, create account UI

--- appengine/main.py
+++ appengine/main.py
@@ -11,6 +11,7 @@ URL_MAPPINGS = [
     (oauth.OAuth2Handler.URL_PATH, oauth.OAuth2Handler),
     (oauth.OAuth2CallbackHandler.URL_PATH, oauth.OAuth2CallbackHandler),
     (login.LoginHandler.URL_PATH, login.LoginHandler),
+    (login.LogoutHandler.URL_PATH, login.LogoutHandler),
     (create_account.CreateAccountHandler.URL_PATH, create_account.CreateAccountHandler)]
 
 def main():
