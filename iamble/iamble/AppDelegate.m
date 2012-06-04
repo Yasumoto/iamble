d5bab24be5c495f306e954ecbaa4c551fcb25941
Joe Smith <yasumoto7@gmail.com>
2012-06-03 23:10:22 -0700

    Re-fix appdelegate....? Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- iamble/iamble/AppDelegate.m
+++ iamble/iamble/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
