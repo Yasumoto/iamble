7b342e8fd5705e657e8cf076eb09a598ab9d5268
Zach Szafran <zszafran@gmail.com>
2012-06-16 20:53:23 -0700

    now looks up manual address for coords

--- appengine/static/scripts/settings.js
+++ appengine/static/scripts/settings.js
@@ -19,13 +19,16 @@
 
   $.cyphtSettings.fn.extend({
     'init': function() {
+      this.geocoder = new google.maps.Geocoder();
       return this;
     },
     'render': function() {
       this.base.render();
       this.options.distance.buttonset();
       this.options.save.button();
-      this.options.static_address.button();
+      this.options.static_address.button().click(
+          this.bind(this.onUseStatic));
+      this.options.address.change(this.bind(this.onUseStatic));
 
       if (!this.options.static_address.is(':checked')) {
         navigator.geolocation.getCurrentPosition(this.bind(this.onPosition));
@@ -33,17 +36,34 @@
       }
       return this;
     },
+    'onUseStatic': function(e) {
+      if (this.options.static_address.is(':checked')) {
+        this.base.options.spinner.fadeIn('fast');
+        this.geocoder.geocode({'address': this.options.address.val()},
+            this.bind(function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                if (typeof(results[0]) != 'undefined') {
+                  this.options.longitude.val(
+                      results[0].geometry.location.lng());
+                  this.options.latitude.val(
+                      results[0].geometry.location.lat());
+                }
+                this.base.options.spinner.fadeOut('fast');
+              }
+            }));
+      }
+    },
     'onPosition': function(position) {
       console.dir(position);
       this.options.longitude.val(position.coords.longitude);
       this.options.latitude.val(position.coords.latitude);
-      var geocoder = new google.maps.Geocoder();
+
       var latlng = new google.maps.LatLng(
         position.coords.latitude,
         position.coords.longitude
       );
 
-      geocoder.geocode({'latLng': latlng}, this.bind(function(results, status) {
+      this.geocoder.geocode({'latLng': latlng}, this.bind(function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           if (typeof(results[0]) != 'undefined') {
             this.options.address.val(results[0].formatted_address);
